436bc49cf9cbc86d562fa03c422dd57ce2a960d3
[dyad] <ukkbayar@gmail.com>
2026-03-11 02:12:00 +0000

    [dyad] Diagnosing startup failure by checking path aliases and version alignment - wrote 4 file(s), added babel-plugin-module-resolver package(s)

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,19 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      'expo-router/babel',
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            '@': './',
+          },
+          extensions: ['.ts', '.tsx', '.js', '.json'],
+        },
+      ],
+    ],
     overrides: [
       {
         // Include @fastshot/* packages for env var inlining
